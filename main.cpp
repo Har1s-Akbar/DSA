5a1658faa8a98a4dcbbe1fc237bbbfe2fec106af
Har1s-Akbar <stanker801@gmail.com>
2026-02-10 23:49:48 +0500

    adding rule of 3

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+
 class vector{
-    int size=0;
-    int capacity=0;
+    int vSize=0;
+    int vCapacity=0;
     int* base_addr = nullptr;
 
     public:
-    // Here  we are initializing the vector with size=0, capacity=0,base_addr=bull_pointer    
-    vector():size(0),capacity(0),base_addr(nullptr){};
+    // Here  we are initializing the vector with vSize=0, vCapacity=0,base_addr=bull_pointer    
+    vector():vSize(0),vCapacity(0),base_addr(nullptr){};
         
     ~vector(){
         delete[] base_addr;
     };
 
 
-    vector(class vector& other):size(other.size),capacity(other.capacity){
-        base_addr = new int[capacity];
-        for(int i = 0; i<size;i++){
+    vector(const vector& other):vSize(other.vSize),vCapacity(other.vCapacity){
+        base_addr = new int[vCapacity];
+        for(int i = 0; i<vSize;i++){
             base_addr[i] = other.base_addr[i];
         };
     };
@@ -25,37 +28,41 @@ class vector{
         if(this == &other)
             return *this;
         
-        delete[] base_addr;
+        // delete[] base_addr;
+        // vCapacity = other.vCapacity;
+        
+        int *temp = new int[other.vCapacity];
 
-        capacity = other.capacity;
-        size = other.size;
-        base_addr = new int[capacity];
-
-        for(int i =0;i<size;i++){
-            base_addr[i] = other.base_addr[i];
+        for(int i =0;i<other.vSize;i++){
+            temp[i] = other.base_addr[i];
         };
+        delete[] base_addr;
+        base_addr = temp;
+        vSize = other.vSize;
+        vCapacity = other.vCapacity;
+        
 
         return *this;
     };
 
         // Here we are creating the push method for vector
         void push(int num){
-            // If capacity is zero or we have null pointer (That means we are initializing the vector)
-            if(capacity == 0){
-                size = 0;
-                capacity = 2;
+            // If vCapacity is zero or we have null pointer (That means we are initializing the vector)
+            if(vCapacity == 0){
+                vSize = 0;
+                vCapacity = 2;
                 // Giving initial values for the vector then the vector will internally calculate 
-                // the size and capacity and resize on it's own
-                base_addr = new int[capacity];
-            } else if (size == capacity){
-                // if our size ==  capacity of memory we will then reallocate the capacity and the memeory
-                capacity = max(1, 2*capacity);
+                // the vSize and vCapacity and resize on it's own
+                base_addr = new int[vCapacity];
+            } else if (vSize == vCapacity){
+                // if our vSize ==  vCapacity of memory we will then reallocate the vCapacity and the memeory
+                vCapacity = std::max(1, 2*vCapacity);
                 // Here we are craeting a temporary pointer so that we can copy the data from 
                 // previous address and then again insert it back after we relocate the address
                 
-                int *temp = new int[capacity];                                      
+                int *temp = new int[vCapacity];                                      
 
-                for(int i=0; i<size ; i++){
+                for(int i=0; i<vSize ; i++){
                     temp[i] = base_addr[i];
                 };
                 // Freeing up the previous memory (essential)
@@ -64,14 +71,43 @@ class vector{
                 base_addr = temp;
             };
             // we need these two lines always no matter what because we have to 
-            // push the given integer and also increment the size.
-            base_addr[size] = num;
-            size++;
+            // push the given integer and also increment the vSize.
+            base_addr[vSize] = num;
+            vSize++;
             
+        }
+        int size() const {
+            return vSize;
+        };
+        int capacity() const{
+            return vCapacity;
+        };
+
+        bool is_empty ()const{
+            return  vSize ? false : true;
         }
 
 };
 
 int main(){
+
+    vector a;
+    a.is_empty() ? cout<<"Vector is empty\n" : cout<<"Vector is not empty\n";
+    a.push(3);
+    a.is_empty() ? cout<<"Vector is empty\n": cout <<"Vector has 1 element\n";
+    a.push(10);
+    a.push(79);
+    a.push(20);
+    a.push(0);
+    int size_of_vector = a.size();
+    int capacity_of_vector = a.capacity();
+    cout<<"The size of the vector is "<< size_of_vector << " The capacity is "<<capacity_of_vector<<"\n";
+
+    vector b;
+    b.is_empty() ? cout<<"Vector is empty\n" : cout<<"Vector is not empty\n";
+    b = a;
+    b.is_empty() ? cout<<"Vector is empty\n" : cout<<"Vector b has elements "<<b.size();
+    
+    // After checking and running it we can say it is working smoothly 
     return 0;
 }
