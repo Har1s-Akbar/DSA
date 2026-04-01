8d254b3c541fae644ed384938e4dc56c0ad93616
Har1s-Akbar <stanker801@gmail.com>
2026-04-01 22:07:31 +0500

    implementing insertAtIndex in circular doubly Linked List

--- circularList/main.cpp
+++ circularList/main.cpp
@@ -30,6 +30,34 @@ class DoublyCircularList{
         newNode->next = head;
         head = newNode;
     };
+
+
+    void insertAtIndex(int index , int num){
+        if(head == nullptr && index!=0){
+            std::cout<<"\nCan not insert at give index because list is empty\n";
+            return;
+        };
+        if(index == 0){
+            insertAtHead(num);
+            return;
+        };
+        Node* temp = head;
+        int i = 0;
+        while(i<index){
+            i++;
+            temp = temp->next;
+            if(temp==head){
+                std::cout<<"Out of bound";
+                return;
+            };
+        };
+        Node* newNode = new Node(num);
+        newNode->next = temp;
+        newNode->prev = temp->prev;
+        temp->prev->next = newNode;
+        temp->prev = newNode;
+    };
+
     void display(){
         if (head== nullptr) return;
         Node* temp = head;
@@ -37,7 +65,9 @@ class DoublyCircularList{
             std::cout<<temp->data<<" ";
             temp = temp->next;
         }while(temp!= head);
+        std::cout<<"\n";
     };
+
     ~DoublyCircularList(){
         if(head == nullptr) return;
         Node* temp = head;
@@ -55,6 +85,10 @@ int main(){
     list.insertAtHead(2);
     list.insertAtHead(5);
     list.insertAtHead(7);
+    list.display();
+    list.insertAtIndex(1,0);
+    list.display();
+    list.insertAtIndex(3,10);
     list.display();
     return 0;
 }
