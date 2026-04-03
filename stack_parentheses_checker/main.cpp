07d2a0d4ac553134d8f70f64072f26729eee1f0c
Har1s-Akbar <stanker801@gmail.com>
2026-04-04 01:14:02 +0500

    implementing parantheses balance checker using stack

---
+++ stack_parentheses_checker/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+struct Node{
+    char data;
+    Node* next;
+
+    Node(char par):data(par){};
+};
+
+class Stack{
+    private:
+    Node* top=nullptr;
+
+    public:
+    void push(char parentheses){
+        Node* newNode = new Node(parentheses);
+        newNode->next = top;
+        top = newNode;
+    };
+
+    void pop(){
+        if(top != nullptr){
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+        }else{
+            std::cout<<"\nAlready Empty\n";
+        };
+    };
+    char peek(){
+        if(top!= nullptr){
+            return top->data;
+        }else{
+            return 0;
+        }
+    };
+    bool isEmpty(){
+        if(top == nullptr){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    ~Stack(){
+        Node* temp = top;
+        Node* tempNext = nullptr;
+
+        while(temp!= nullptr){
+            tempNext = temp->next;
+            delete temp;
+            temp = tempNext;
+        };
+    };
+};
+
+
+
+int main() {
+    std::string myExpr = "{[()]}";
+
+    std::cout << "Testing expression: " << myExpr << std::endl;
+
+    if (isBalanced(myExpr)) {
+        std::cout << "Result: The brackets are Balanced!" << std::endl;
+    } else {
+        std::cout << "Result: The brackets are Unbalanced!" << std::endl;
+    }
+
+    return 0;
+};
