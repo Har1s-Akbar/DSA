cd8b23e6861c0f2697d9e44edd8f942ada38ec58
Har1s-Akbar <stanker801@gmail.com>
2026-04-09 23:04:31 +0500

    adding methods

---
+++ window_queue_prob/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+struct Node{
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int num):data(num),next(nullptr), prev(nullptr){};
+};
+
+class Dequeue{
+    private:
+    Node* front=nullptr;
+    Node*rear = nullptr;
+
+    public:
+    void enqueue(int num){
+        Node* newNode = new Node(num);
+        if(front == nullptr){    
+            newNode->next = front;
+            front = newNode;
+            rear = newNode;
+            return;
+        }else{
+            rear->next =newNode;
+            newNode->prev = rear;
+            rear = newNode;
+        }
+
+        void dequeue(){
+            if(front == nullptr){
+
+            }
+        }
+    }
+};
