8b9917a0d3a9b29c9236e235548113a3c9d426a0
Har1s-Akbar <stanker801@gmail.com>
2026-04-25 00:48:53 +0500

    implementing heap for priority queue constructing proper heap

--- Priority_Queue/main.cpp
+++ Priority_Queue/main.cpp
@@ -14,10 +14,21 @@ class Myheap{
     int right(int i){
         return (2*i+2);
     }
+
+    void heapifyUp(int num){
+        if(heap.size() == 0){
+            heap.insert(heap.begin(),num);
+        }else{
+            heap.push_back(num);
+            
+        }
+    }
+
+
     public:
 
     void insert(int num){
-        
+        heapifyUp(num);        
     }
     
 }
