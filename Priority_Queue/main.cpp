3a69c315b81cf2ce32f5fddb195de9eba97d04b8
Har1s-Akbar <stanker801@gmail.com>
2026-04-25 17:38:14 +0500

    heapify down utility  function for pop in heap

--- Priority_Queue/main.cpp
+++ Priority_Queue/main.cpp
@@ -23,12 +23,30 @@ class Myheap{
         }
     }
 
+    void heapifyDown(int index){
+        int startIndex = 0;
+        while(index>startIndex){
+            if(heap[startIndex]<heap[startIndex+1]){
+                std::swap(heap[startIndex],heap[startIndex+1]);
+                startIndex = 2*index+1;
+            };
+            if(heap[startIndex]<heap[startIndex+2]){
+                std::swap(heap[startIndex],heap[startIndex+2]);
+                startIndex =2*index+2;
+            };
+        }
+    }
 
     public:
 
     void insert(int num){
         heap.push_back(num);
         heapifyUp(heap.size()-1);
-    }        
-    
+    };
+    void pop(){
+        heap[0] = heap[heap.size()-1];
+        heap.erase(heap.begin()+heap.size()-1);
+        int index = heap.size()-1;
+        heapifyDown(index);
+    }    
 }
