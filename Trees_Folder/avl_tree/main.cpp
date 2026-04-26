0158bb60fedf5884db38bc7d50ea43e307b45883
Har1s-Akbar <stanker801@gmail.com>
2026-04-27 00:47:08 +0500

    implmenting the bst insertion and getting the height of the tree

--- Trees_Folder/avl_tree/main.cpp
+++ Trees_Folder/avl_tree/main.cpp
@@ -12,16 +12,20 @@ class Avl{
     private:
         Node* root=nullptr;
 
-        Node* catchImbalance(Node*& temp){
-            if(temp == nullptr){
-                return nullptr;
+        void nodeRotation(Node*& temp, int num){
+            if(root == nullptr){
+                return;
             }else{
-                Node* imbNode = catchImbalance(temp->left);
-                
-            }
-        };
-        void nodeRotation(Node*& temp){
-            Node* imbalancedNode = catchImbalance(temp);
+                Node* tempPointer = root;
+                if(tempPointer->data>num){
+                    nodeRotation(tempPointer->left, num);
+                }else if(tempPointer->data == num){
+                    std::cout<<"Can not insert a duplicate entry";
+                    return;
+                }else{
+                    nodeRotation(tempPointer->right,num);
+                }
+            };
         };
 
     public:
@@ -30,23 +34,8 @@ class Avl{
             if(root == nullptr){
                 root = newNode;
             }else if(root != nullptr){
-                Node* temp = root;
-                while(temp!= nullptr){
-                    if(temp->data > num){
-                        temp = temp->left;
-                    }else if(temp->data == num){
-                        std::cout<<"Can not insert Duplicates";
-                        delete newNode;
-                        return;
-                    }else{
-                        temp = temp->right;
-                    }
-                }
+                nodeRotation(newNode, num);
             };
-
-            Node* temp = root;
-            nodeRotation(temp);
-
         };
         
 };
