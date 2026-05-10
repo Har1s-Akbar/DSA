2d0000a5145424eb9c269f12158abdc269a1435e
Har1s-Akbar <stanker801@gmail.com>
2026-05-10 14:35:11 +0500

    adding itteration and printing

--- graphs/unordered_unweighted_graph/main.cpp
+++ graphs/unordered_unweighted_graph/main.cpp
@@ -12,4 +12,29 @@ class Graph{
         graph[baseVertex].push_back(targetVertex);
         graph[targetVertex].push_back(baseVertex);
     };
+    void printVertices(){
+        if(graph.empty()){
+            std::cout<<"No vertices\n";
+        }
+
+        for(const auto& [vertex, neighbours] : graph){
+            std::cout<<"\n-"<<vertex<<"\n";
+            for(const auto& x:neighbours){
+                std::cout<<x<<" ";
+            }
+        };
+    }
 };
+
+
+int main() {
+    Graph mySocialNetwork;
+
+    mySocialNetwork.addEdge("Alice", "Bob");
+    mySocialNetwork.addEdge("Alice", "Charlie");
+    mySocialNetwork.addEdge("Bob", "David");
+
+    mySocialNetwork.printVertices();
+
+    return 0;
+}
